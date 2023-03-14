22ec485e22b0091ab5f96f2ee4a68a4a38992c08
Talia Kaza <122602912+TaliaKaza@users.noreply.github.com>
2023-03-14 03:08:16 +0300

    Update ERC721-mint-nft-token.sol

--- ERC721-mint-nft-token.sol
+++ ERC721-mint-nft-token.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Masternft is ERC721, ERC721URIStorage, Ownable {
+contract TaliaK is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
